6f80c5c061e98efdcf47aab92dcb7bc1becd56d8
honeyonsys <honeyonsys@gmail.com>
2022-04-10 18:07:35 +0530

    authrization and callback handler

--- src/main/java/com/taomish/xero/XeroInvoiceController.java
+++ src/main/java/com/taomish/xero/XeroInvoiceController.java
@@ -1,15 +1,24 @@
 package com.taomish.xero;
 
+import java.io.IOException;
+import java.util.*;
+
+import javax.servlet.http.*;
+
 import com.taomish.xero.dto.InvoiceDTO;
 import com.taomish.xero.service.XeroInvoiceService;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+@Controller
 public class XeroInvoiceController {
 
   private final XeroInvoiceService xeroInvoiceService;
+  private String XeroClientId = "97C62FF3B0C7417393BC515F35A0554E";
+  private String XeroClientSecret = "b6mIuZ7Kz0H1wQn_HjL1jGvKcyT2G16_YBJNXxcaPV1CAw7i";
+  private String XeroRedirectURL = "http://localhost:8080/xero/callback";
+  private String XeroAPIScopes = "openid profile email accounting.transactions accounting.settings accounting.contacts";
 
   public XeroInvoiceController(XeroInvoiceService xeroInvoiceService) {
     this.xeroInvoiceService = xeroInvoiceService;
@@ -19,4 +28,57 @@ public class XeroInvoiceController {
 	public InvoiceDTO submitInvoice(@RequestBody InvoiceDTO invoice) {
 		return xeroInvoiceService.submitInvoice(invoice);
 	}
+  
+  @GetMapping("/xero/authorization")
+	public void authenticateXero(HttpServletResponse response) {
+		//return "authorization";
+    try {
+      String secretState = "secret" + new Random().nextInt(999_999);
+      String authURL = "https://login.xero.com/identity/connect/authorize?response_type=code&client_id="+this.XeroClientId+"&redirect_uri="+this.XeroRedirectURL+"&scope="+this.XeroAPIScopes+"&state="+secretState;
+      this.saveCookie(response, "state", secretState);
+      response.sendRedirect(authURL);
+    } catch (IOException e) {
+      // TODO Auto-generated catch block
+      e.printStackTrace();
+    }
+    
+	}
+
+  @GetMapping("/xero/callback")
+  public String xeroCallback(HttpServletRequest request) {
+    String secretState = getCookie(request, "state");
+    String callbackCode = "";
+    if (request.getParameter("code") != null) {
+      callbackCode = request.getParameter("code");
+    }
+    if (request.getParameter("state") != null && secretState.equals(request.getParameter("state").toString())) {
+    
+      System.out.println("state matched-----");
+
+
+    } else {
+      System.out.println("Invalid state - possible CSFR");
+    }
+    return "Processing your request...";
+  }
+
+  //Creating cookie function
+  public void saveCookie(HttpServletResponse response, String key, String value) {
+    Cookie t = new Cookie(key, value);
+    response.addCookie(t);
+  }
+
+  //Getting value of saved cookie
+  public String getCookie(HttpServletRequest request, String key) {
+    String item = null;
+    Cookie[] cookies = request.getCookies();
+    if (cookies != null) {
+        for (int i = 0; i < cookies.length; i++) {
+            if (cookies[i].getName().equals(key)) {
+                item = cookies[i].getValue();
+            }
+        }
+    }
+    return item;
+}
 }
